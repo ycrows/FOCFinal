3c962efdc32a50fabc1ae7192e529988098da8a0
ycrows <155534377+ycrows@users.noreply.github.com>
2025-12-09 15:17:15 +0800

    main.c prototype

--- main.c
+++ main.c
@@ -1,8 +1,30 @@
 #include <stdio.h>
-
-int main() {
-    // Write C code here
-    printf("HEllo WOrld!\n");
-
-    return 0;
+#include "user_register/register.h"
+int main(int argc, char *argv[])
+{
+	int login_choice = 0;
+    printf("==================== Login ====================\n");
+    printf("1. Login\n");
+    printf("2. Register (If you do not have an account)\n");
+	printf("3. Exit\n");
+	printf("===============================================\n");
+	printf("Choose an option (1-3): ");
+	
+	while (login_choice != 1 && login_choice != 2 && login_choice != 3) {
+		scanf("%d", &login_choice);
+		if (login_choice == 1) {
+			//user_service();
+			printf("placeholder\n");
+			return 0;
+		}
+		else if (login_choice == 2) {
+			user_register();
+			return 0;
+		}
+		else if (login_choice == 3)
+			return 0;
+	    else
+			printf("Invalid choice! Please enter a number between 1 and 3.\n");
+	}
 }
+    
