4d44428346c4b58ec73f3f0b4d0dee0086aeb2da
Darren <boiapsmdoais@gmail.com>
2025-12-10 00:18:36 +0800

    added workflow for everyone

--- user_register/user_register.c
+++ user_register/user_register.c
@@ -1,4 +1,18 @@
 #include <stdio.h>
 void user_register(){
-    printf("hello world!");
+    print("hello world!\n");
+    // don't change the variables pleaese
+    char user[];
+    char password[];
+    char password_confirm[];
+    // put code below to get the username
+
+    // put code below to get the password
+
+    // confirm the password / check the password
+
+    // check if user in the database
+
+    // if user and password is correct
+    
 }
