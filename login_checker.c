8de02664588210cec0ff7b14b5f33b5ee9b87ee3
Darren <boiapsmdoais@gmail.com>
2025-12-13 20:29:54 +0800

    added code to check login

--- login_checker.c
+++ login_checker.c
@@ -9,6 +9,7 @@ int login_checker(char username[], char password[]){
     char file_password[50];
     int user_found = 0;
 
+    // in the case there are no users registered yet
     if (fgets(line, sizeof(line), fp) == NULL) {
         printf("No user in this system, please register one.\n");
         fclose(fp);
@@ -18,15 +19,15 @@ int login_checker(char username[], char password[]){
     while (fgets(line, sizeof(line), fp)) { // iterate through every line
         // find the names in database by searching for name=
         if (strncmp(line, "name=", 5) == 0) {
-            sscanf(line, "name=%49s", file_username);
+            sscanf(line, "name=%49s", file_username); // store the username
 
-            // if username matches
+            // check username matches
             if (strcmp(file_username, username) == 0) {
                 user_found = 1; // mark user found
                 // read next line password=
                 if (fgets(line, sizeof(line), fp)) {
-                    sscanf(line, "password=%49s", file_password);
-                    if (strcmp(file_password, password) == 0) {
+                    sscanf(line, "password=%49s", file_password); // store the password
+                    if (strcmp(file_password, password) == 0) { // check password matches
                         printf("Login successful. Welcome %s!\n", username);
                         return 1;
                     } else {
