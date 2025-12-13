f67f77b700e605a06af4a69118c6c391f1a8f36e
duncanhypon <duncanhypon@gmail.com>
2025-12-13 14:27:40 +0800

    Update save_to_database.c
    added

--- user_register/save_to_database.c
+++ user_register/save_to_database.c
@@ -1,13 +1,15 @@
 void save_to_database(char username[], char password[]) {
   File *database
   database=fopen("database.txt", "a")
-  if(fp==NULL)
+  if(database==NULL){
   return 0;
-fprintf(fp,"name%s\n",username);\
+  }
+fprintf(fp,"name%s\n",username);
 fprintf(fp,"password=%s\n",password);
 fprintf(fp,"friends=\n");
 fprintf(fp,"incoming_request=\n");
 fprintf(fp,"outgoing_request=\n");
 fprintf(database, "\n");
-fclose(fp)
+fclose(database);
+ return 0;
 }
